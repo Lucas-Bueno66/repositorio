67de5135f26f3fe0af71734de581733600701021
Lucas Bueno <lpassisbueno@gmail.com>
2024-11-06 17:05:08 -0300

    alteração

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.post('/scrape', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/google-chrome-stable',
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
